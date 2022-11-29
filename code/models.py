d69a9e7c34409c65a009122f4d83ad8c4de9a643
kendraleek <kendralee@kendras-air-2.devices.brown.edu>
2022-11-29 18:37:40 -0500

    putting in code from CNN

---
+++ code/models.py
@@ -0,0 +1,108 @@
+"""
+Homework 5 - CNNs
+CS1430 - Computer Vision
+Brown University
+"""
+
+import tensorflow as tf
+from tensorflow.keras.layers import \
+    Conv2D, MaxPool2D, Dropout, Flatten, Dense
+
+import hyperparameters as hp
+
+
+class VGGModel(tf.keras.Model):
+    def __init__(self):
+        super(VGGModel, self).__init__()
+
+        # TASK 3
+        # TODO: Select an optimizer for your network (see the documentation
+        #       for tf.keras.optimizers)
+
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate=hp.learning_rate)
+
+        # Don't change the below:
+
+        self.vgg16 = [
+            # Block 1
+            Conv2D(64, 3, 1, padding="same",
+                   activation="relu", name="block1_conv1"),
+            Conv2D(64, 3, 1, padding="same",
+                   activation="relu", name="block1_conv2"),
+            MaxPool2D(2, name="block1_pool"),
+            # Block 2
+            Conv2D(128, 3, 1, padding="same",
+                   activation="relu", name="block2_conv1"),
+            Conv2D(128, 3, 1, padding="same",
+                   activation="relu", name="block2_conv2"),
+            MaxPool2D(2, name="block2_pool"),
+            # Block 3
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv1"),
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv2"),
+            Conv2D(256, 3, 1, padding="same",
+                   activation="relu", name="block3_conv3"),
+            MaxPool2D(2, name="block3_pool"),
+            # Block 4
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv1"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv2"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block4_conv3"),
+            MaxPool2D(2, name="block4_pool"),
+            # Block 5
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv1"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv2"),
+            Conv2D(512, 3, 1, padding="same",
+                   activation="relu", name="block5_conv3"),
+            MaxPool2D(2, name="block5_pool")
+        ]
+
+        # TASK 3
+        # TODO: Make all layers in self.vgg16 non-trainable. This will freeze the
+        #       pretrained VGG16 weights into place so that only the classificaiton
+        #       head is trained.
+        for i in range(len(self.vgg16)):
+              self.vgg16[i].trainable = False
+
+        # TODO: Write a classification head for our 15-scene classification task.
+
+       #  self.head = [
+       #        tf.keras.layers.Flatten(),
+       #        tf.keras.layers.Dense(550, activation="relu"),
+       #        tf.keras.layers.Dense(70, activation="relu"),
+       #        tf.keras.layers.Dense(15, activation="softmax")
+       #  ]
+        self.head = [
+              Dense(256, activation='relu'),
+              Flatten(),
+              Dropout(0.3),
+              Dense(hp.num_classes, activation='softmax')
+        ]
+
+        # Don't change the below:
+        self.vgg16 = tf.keras.Sequential(self.vgg16, name="vgg_base")
+        self.head = tf.keras.Sequential(self.head, name="vgg_head")
+
+    def call(self, x):
+        """ Passes the image through the network. """
+
+        x = self.vgg16(x)
+        x = self.head(x)
+
+        return x
+
+    @staticmethod
+    def loss_fn(labels, predictions):
+        """ Loss function for model. """
+
+        # TASK 3
+        # TODO: Select a loss function for your network (see the documentation
+        #       for tf.keras.losses)
+
+        loss = tf.keras.losses.sparse_categorical_crossentropy(labels, predictions)
+        return loss
