3598343ad1935c1f5bba0e82eba14f7353023935
kendraleek <kendralee@kendras-air-2.devices.brown.edu>
2022-12-11 21:58:38 -0500

    kendra - testy

--- code/run.py
+++ code/run.py
@@ -61,6 +61,7 @@ def main():
     TRAIN_PATH = ('../data/train/')
     TEST_PATH = ('../data/test/')
     MY_TEST_PATH = ('../data/my_test/')
+    TESTY_PATH = ('../data/testy/')
     training_data = Datasets(TRAIN_PATH, hp.img_size)
     X_train, y_train, train_labels = training_data.load_data()
     print('Our Training Labels: ')
@@ -68,7 +69,8 @@ def main():
     print()
 
 
-    testing_data = Datasets(MY_TEST_PATH, hp.img_size)
+    # testing_data = Datasets(MY_TEST_PATH, hp.img_size)
+    testing_data = Datasets(TESTY_PATH, hp.img_size)
     X_test, y_test, testing_labels = testing_data.load_data()
 
     model = VGGModel()
@@ -88,7 +90,7 @@ def main():
         print('Test accuracy... = %.2f' % accuracy_score(y_test, preds))
     elif ARGS.makePredictions:
         data_gen = ImageDataGenerator(rescale = 1.0/255)
-        pred_gen = data_gen.flow_from_directory(MY_TEST_PATH, target_size = hp.img_size, color_mode = "grayscale", batch_size = hp.batch_size, class_mode = "categorical", shuffle = False)
+        pred_gen = data_gen.flow_from_directory(TESTY_PATH, target_size = hp.img_size, color_mode = "grayscale", batch_size = hp.batch_size, class_mode = "categorical", shuffle = False)
         preds = test(model, X_test)
         preds = [testing_labels[l] for l in preds]
         files = pred_gen.filenames
@@ -96,7 +98,7 @@ def main():
         results = pd.DataFrame({"file": files, "predictions": preds, "actual label": actual_label})
         for i in range(len(files)):
             file, prediction, actual = results.loc[i]
-            path = os.path.join(MY_TEST_PATH, file)
+            path = os.path.join(TESTY_PATH, file)
             img = cv2.imread(path)
             plt.imshow(img)
             plt.title("Our model predicted class: {} {} Actual class: {}".format(prediction, '\n', actual))
